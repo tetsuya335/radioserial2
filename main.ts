becd2c72eef28f8340871b2ec9b9dc982cc9240b
tetsuya335 <tmgzate@gmail.com>
2026-06-02 18:25:31 +0900

    STATとSTOPのアイコンを変更

--- main.ts
+++ main.ts
@@ -1,26 +1,33 @@
-radio.setGroup(1)
-serial.redirectToUSB()
-basic.clearScreen() // 【変更】最初は画面に何も表示しない
-
+// 【変更】最初は画面に何も表示しない
 input.onButtonPressed(Button.A, function () {
     serial.writeLine("START")
-    basic.showIcon(IconNames.Heart)  // 【変更】受信したらハートを表示
-    basic.pause(500)                 // 【変更】0.5秒（500ミリ秒）そのまま待つ
-    basic.clearScreen()              // 【変更】画面を消す
+    // 【変更】受信したらハートを表示
+    basic.showIcon(IconNames.EighthNote)
+    // 【変更】0.5秒（500ミリ秒）そのまま待つ
+    basic.pause(500)
+    // 【変更】画面を消す
+    basic.clearScreen()
 })
-
-input.onButtonPressed(Button.B, function () {
-    serial.writeLine("STOP")
-    basic.showIcon(IconNames.Heart)  // 【変更】受信したらハートを表示
-    basic.pause(500)                 // 【変更】0.5秒（500ミリ秒）そのまま待つ
-    basic.clearScreen()              // 【変更】画面を消す
-})
-
 // 無線を受信したときの処理
 radio.onReceivedString(function (receivedString) {
     serial.writeLine(receivedString)
-    
-    basic.showIcon(IconNames.Heart)  // 【変更】受信したらハートを表示
-    basic.pause(500)                 // 【変更】0.5秒（500ミリ秒）そのまま待つ
-    basic.clearScreen()              // 【変更】画面を消す
+    // 【変更】受信したらハートを表示
+    basic.showIcon(IconNames.Heart)
+    // 【変更】0.5秒（500ミリ秒）そのまま待つ
+    basic.pause(500)
+    // 【変更】画面を消す
+    basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    serial.writeLine("STOP")
+    // 【変更】受信したらハートを表示
+    basic.showIcon(IconNames.Yes)
+    // 【変更】0.5秒（500ミリ秒）そのまま待つ
+    basic.pause(500)
+    // 【変更】画面を消す
+    basic.clearScreen()
+})
+radio.setGroup(1)
+serial.redirectToUSB()
+// 【変更】最初は画面に何も表示しない
+basic.clearScreen()
